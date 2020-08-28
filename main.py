36128a2f107c9ca58c8932ea65ef5d455eb4edfb
seanmurphy611 <spm6407@psu.edu>
2020-08-28 23:18:59 +0000

    case sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 c = 0.0
 f = 0.0
-c = float(input("Enter temperature in Celsius: "))
-f = float(c*1.8+32)
+c = float(input("Enter temperature in celsius: "))
+f = (c*1.8+32)
 print( str(c) + u"\N{DEGREE SIGN}" + " in Celsius is equivalent to "+ str(f)+"° Fahrenheit.")
 
