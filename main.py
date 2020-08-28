3230a79a9506a62a2052b1a5c7f3e19db7fada47
seanmurphy611 <spm6407@psu.edu>
2020-08-28 22:32:04 +0000

    string in statement

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ c = 0.0
 f = 0.0
 c = float(input("Enter temperature in Celsius: "))
 f = float(c*1.8+32)
-print(c,"° in Celsius is equivalent to ", f , "° Fahrenheit")
+print( str(c) + u"\N{DEGREE SIGN}" + " in Celsius is equivalent to "+ str(f)+"° Fahrenheit")
+
